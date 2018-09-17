3c1ac1616ebc5b488f0cc540663d4dce2f7ecce5
eveoliv <everton_ace@hotmail.com>
2018-09-17 20:12:39 -0300

    Implementacao das views de frequencia

--- ChamadaQR/Controllers/FrequenciaController.cs
+++ ChamadaQR/Controllers/FrequenciaController.cs
@@ -25,5 +25,54 @@ namespace ChamadaQR.Controllers
         {
             return View(await frequenciaDAL.ObterFrequenciasClassificadasPorNome().ToListAsync());
         }
+
+        private async Task<IActionResult> ObterVisaoFrequenciaPorId(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var frequencia = await frequenciaDAL.ObterFrequenciaPorID((long)id);
+            if (frequencia == null)
+            {
+                return NotFound();
+            }
+            return View(frequencia);
+        }
+
+        public async Task<IActionResult> Detail(long? id)
+        {
+            return await ObterVisaoFrequenciaPorId(id);
+        }
+
+        public async Task<IActionResult> Edit(long id)
+        {
+            return await ObterVisaoFrequenciaPorId(id);
+        }
+
+        public async Task<IActionResult> Delete(long? id)
+        {
+            return await ObterVisaoFrequenciaPorId(id);
+        }
+
+        // GET: Frequencia/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //var frequencia = await _context.Frequencias.Include(d => d.Alunos).SingleOrDefaultAsync(m => m.ProjetoID == id);
+            var frequencia = await _context.Frequencias.Where(f => f.FrequenciaID == id)
+                                                       .Include(a => a.Aluno)
+                                                       .Include(d => d.Calendario).ToListAsync();
+            if (frequencia == null)
+            {
+                return NotFound();
+            }
+
+            return View(frequencia);
+        }
     }
 }
