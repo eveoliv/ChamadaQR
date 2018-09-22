08149925151e9a02de003f94fc33e67955c74a67
eveoliv <everton_ace@hotmail.com>
2018-09-22 09:33:20 -0300

    Professor.cs

---
+++ Models/Cadastros/Professor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Modelo.Cadastros
+{
+    class Professor
+    {
+        [Key]
+        public int ProfessorID { get; set; }
+        public long Matricula { get; set; }
+        public string ProfessorNome { get; set; }
+        public string Status { get; set; }
+
+        //fk
+        public long? ProjetoID { get; set; }
+        public Projeto Projeto { get; set; }
+
+    }
+}
